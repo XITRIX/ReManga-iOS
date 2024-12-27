9d7cfa88e89e692b0027e49726f4affcd1d9ce45
Ilya Semenow <isemenow@teladochealth.com>
2024-12-27 14:22:40 +0100

    Revert "Add proper catch for all cases in fetching category for remanga"
    This reverts commit fd237195b86bf9ea210d6aa5452c734b8a6f5179.

--- ReManga/Api/ReManga/ReMangaApi.swift
+++ ReManga/Api/ReManga/ReMangaApi.swift
@@ -98,22 +98,19 @@ class ReMangaApi: ApiProtocol {
         }
 
         let url = "https://api.remanga.org/api/search/catalog/?count=30&ordering=-\(sorting.id)&page=\(page)\(tags)"
-        do {
-            let model: ReMangaApiMangaCatalogResult = try await performRequest(makeRequest(url))
-            var bookmarks: [ApiMangaBookmarkModel] = []
-            bookmarks = try await fetchBookmarkTypes()
+        let model: ReMangaApiMangaCatalogResult = try await performRequest(makeRequest(url))
 
-            let res = model.content.map { model in
-                var item = ApiMangaModel(from: model)
-                item.bookmark = bookmarks.first(where: { model.bookmarkType == $0.name })
-                return item
-            }
+        var bookmarks: [ApiMangaBookmarkModel] = []
+        do { bookmarks = try await fetchBookmarkTypes() }
+        catch { print(error) }
 
-            return res
-        } catch {
-            print(error)
+        let res = model.content.map { model in
+            var item = ApiMangaModel(from: model)
+            item.bookmark = bookmarks.first(where: { model.bookmarkType == $0.name })
+            return item
         }
-        return []
+
+        return res
     }
 
     func fetchSearch(query: String, page: Int, sorting: ApiMangaIdModel) async throws -> [ApiMangaModel] {
